0f53facbb9d8fa681fb0d59c50ab58fbd54f0011
PaQPaweł <paq-pawel1996@o2.pl>
2020-05-18 22:33:56 +0200

    Zadanie33

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
+document.addEventListener("click", AddText);
+function AddText() {
+    document.getElementById("opis").innerHTML = "Akademia 108";
+}
+
+
+
+
+/* to wyzej mniej w HTML'u
+LUB
 var par=document.getElementById('opis');
 var button=document.querySelector('#opis');
 function Dodaj()
 {
     document.write("Akademia 108");
-}
+}*
+/
